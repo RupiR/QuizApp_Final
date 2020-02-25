9b1b9211faef25ae8a3ab63c2ab21114f12310a0
Rupali <rupalirajput@gmail.com>
2020-02-25 17:22:11 -0600

    added answer status page

--- index.js
+++ index.js
@@ -34,66 +34,67 @@ let questions = [
       "Shakespeare In Love"
     ],
     correct: 0
-  },
-  {
-    title:
-      "Boyz II Men sang the praises of what city, their hometown, in their debut 1991 single?",
-    answers: ["Omaha", "Philadelphia", "Atlanta", "Boston"],
-    correct: 1
-  },
-  {
-    title:
-      'What 1990 M.C. Hammer hit featured a sample from Rick James\' "Super Freak"?',
-    answers: [
-      "Dancing Machine",
-      "They Put Me In The Mix",
-      "U Can't Touch This",
-      "2 Legit 2 Quit"
-    ],
-    correct: 2
-  },
-  {
-    title:
-      'Which 1990 rap hit featured a sample of "Under Pressure" from Queen and David Bowie?',
-    answers: [
-      "Cool As Ice",
-      "Ice Ice Baby",
-      "Fight The Power",
-      "Play That Funky Music"
-    ],
-    correct: 1
-  },
-  {
-    title:
-      'Ironic was one of five hit singles from which artist\'s "Jagged Little Pill" album?',
-    answers: [
-      "Celine Dion",
-      "Alanis Morissette",
-      "Mariah Carey",
-      "Martha Wash"
-    ],
-    correct: 1
-  },
-  {
-    title:
-      "Dr. Dre's \"Nuthin' But a 'G' Thang\" featured an appearance by what up-and-coming rapper?",
-    answers: ["Jay-Z", "Nas", "Snoop Doggy Dogg", "Tupac Shakur"],
-    correct: 2
-  },
-  {
-    title:
-      "The Latin duo Los Del Rio popularized what dance craze with a 1996 Spanish-language hit?",
-    answers: [
-      "The Macarena",
-      "The Cha-Cha Slide",
-      "The Chicken Dance",
-      "The Achy Breaky"
-    ],
-    correct: 0
   }
+  // {
+  //   title:
+  //     "Boyz II Men sang the praises of what city, their hometown, in their debut 1991 single?",
+  //   answers: ["Omaha", "Philadelphia", "Atlanta", "Boston"],
+  //   correct: 1
+  // },
+  // {
+  //   title:
+  //     'What 1990 M.C. Hammer hit featured a sample from Rick James\' "Super Freak"?',
+  //   answers: [
+  //     "Dancing Machine",
+  //     "They Put Me In The Mix",
+  //     "U Can't Touch This",
+  //     "2 Legit 2 Quit"
+  //   ],
+  //   correct: 2
+  // },
+  // {
+  //   title:
+  //     'Which 1990 rap hit featured a sample of "Under Pressure" from Queen and David Bowie?',
+  //   answers: [
+  //     "Cool As Ice",
+  //     "Ice Ice Baby",
+  //     "Fight The Power",
+  //     "Play That Funky Music"
+  //   ],
+  //   correct: 1
+  // },
+  // {
+  //   title:
+  //     'Ironic was one of five hit singles from which artist\'s "Jagged Little Pill" album?',
+  //   answers: [
+  //     "Celine Dion",
+  //     "Alanis Morissette",
+  //     "Mariah Carey",
+  //     "Martha Wash"
+  //   ],
+  //   correct: 1
+  // },
+  // {
+  //   title:
+  //     "Dr. Dre's \"Nuthin' But a 'G' Thang\" featured an appearance by what up-and-coming rapper?",
+  //   answers: ["Jay-Z", "Nas", "Snoop Doggy Dogg", "Tupac Shakur"],
+  //   correct: 2
+  // },
+  // {
+  //   title:
+  //     "The Latin duo Los Del Rio popularized what dance craze with a 1996 Spanish-language hit?",
+  //   answers: [
+  //     "The Macarena",
+  //     "The Cha-Cha Slide",
+  //     "The Chicken Dance",
+  //     "The Achy Breaky"
+  //   ],
+  //   correct: 0
+  // }
 ];
 
 $(document).ready(function() {
+  $(".feedback").hide();
   $(".start a").click(function(e) {
     e.preventDefault();
     $(".start").hide();
@@ -101,19 +102,26 @@ $(document).ready(function() {
     showQuestion();
   });
 
-  $(".quiz ul").on("click", "li", function() {
-    $(".selected").removeClass("selected");
-    $(this).addClass("selected");
+  // $(".quiz ul").on("click", "li", function() {
+  //   $(".selected").removeClass("selected");
+  //   $(this).addClass("selected");
+  // });
+
+  $("#nextquestion").click(e => {
+    $(".feedback").hide();
+    $(".quiz").show();
+    showQuestion();
   });
 
-  $(".quiz a").click(function(e) {
+  $("#questionform").submit(function(e) {
     e.preventDefault();
-    if ($("li.selected").length) {
-      let guess = parseInt($("li.selected").attr("id"));
+    let guess = e.target.answers.value;
+    if (guess) {
       checkAnswer(guess);
     } else {
       alert("Please Select An Answer");
     }
+    e.target.reset();
   });
 
   $(".summary a").click(function(e) {
@@ -124,31 +132,44 @@ $(document).ready(function() {
 
 function showQuestion() {
   let question = questions[currentQuestion];
-  $(".quiz h2").text(question.title);
-  $(".quiz ul").html("");
+  $("form legend").text(question.title);
   $(".quiz h6").text(
     " Question: " + [currentQuestion + 1] + " / " + questions.length
   );
   $(".quiz h5").text("Score: " + score + " / " + questions.length);
   for (var i = 0; i < question.answers.length; i++) {
-    $(".quiz ul").append("<li id='" + i + "'>" + question.answers[i] + "</li>");
+    $("#label" + i).text(question.answers[i]);
   }
 }
 
 function checkAnswer(guess) {
   let question = questions[currentQuestion];
-  if (question.correct === guess) {
+  let feedback = false;
+  if (question.correct === parseInt(guess)) {
     score++;
+    feedback = true;
   }
   currentQuestion++;
   if (currentQuestion >= questions.length) {
     showSummary();
   } else {
-    showQuestion();
+    showFeedback(feedback);
   }
 }
+
+function showFeedback(feedback) {
+  $(".quiz").hide();
+  $(".feedback").show();
+  if (feedback) {
+    $("#feedbacktext").text("You answered correctly!");
+  } else {
+    $("#feedbacktext").text("You answered incorrectly!");
+  }
+}
+
 function showSummary() {
   $(".quiz").hide();
+  $(".feedback").hide();
   $(".summary").show();
   $(".summary p").text(
     "Congrats, you scored " + score + " out of " + questions.length + " !"
