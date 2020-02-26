d58aa562c08aac1e3ec0c4c86efca72dde54a213
Rupali <rupalirajput@gmail.com>
2020-02-25 18:00:10 -0600

    fixed bugs

--- index.js
+++ index.js
@@ -34,63 +34,63 @@ let questions = [
       "Shakespeare In Love"
     ],
     correct: 0
+  },
+  {
+    title:
+      "Boyz II Men sang the praises of what city, their hometown, in their debut 1991 single?",
+    answers: ["Omaha", "Philadelphia", "Atlanta", "Boston"],
+    correct: 1
+  },
+  {
+    title:
+      'What 1990 M.C. Hammer hit featured a sample from Rick James\' "Super Freak"?',
+    answers: [
+      "Dancing Machine",
+      "They Put Me In The Mix",
+      "U Can't Touch This",
+      "2 Legit 2 Quit"
+    ],
+    correct: 2
+  },
+  {
+    title:
+      'Which 1990 rap hit featured a sample of "Under Pressure" from Queen and David Bowie?',
+    answers: [
+      "Cool As Ice",
+      "Ice Ice Baby",
+      "Fight The Power",
+      "Play That Funky Music"
+    ],
+    correct: 1
+  },
+  {
+    title:
+      'Ironic was one of five hit singles from which artist\'s "Jagged Little Pill" album?',
+    answers: [
+      "Celine Dion",
+      "Alanis Morissette",
+      "Mariah Carey",
+      "Martha Wash"
+    ],
+    correct: 1
+  },
+  {
+    title:
+      "Dr. Dre's \"Nuthin' But a 'G' Thang\" featured an appearance by what up-and-coming rapper?",
+    answers: ["Jay-Z", "Nas", "Snoop Doggy Dogg", "Tupac Shakur"],
+    correct: 2
+  },
+  {
+    title:
+      "The Latin duo Los Del Rio popularized what dance craze with a 1996 Spanish-language hit?",
+    answers: [
+      "The Macarena",
+      "The Cha-Cha Slide",
+      "The Chicken Dance",
+      "The Achy Breaky"
+    ],
+    correct: 0
   }
-  // {
-  //   title:
-  //     "Boyz II Men sang the praises of what city, their hometown, in their debut 1991 single?",
-  //   answers: ["Omaha", "Philadelphia", "Atlanta", "Boston"],
-  //   correct: 1
-  // },
-  // {
-  //   title:
-  //     'What 1990 M.C. Hammer hit featured a sample from Rick James\' "Super Freak"?',
-  //   answers: [
-  //     "Dancing Machine",
-  //     "They Put Me In The Mix",
-  //     "U Can't Touch This",
-  //     "2 Legit 2 Quit"
-  //   ],
-  //   correct: 2
-  // },
-  // {
-  //   title:
-  //     'Which 1990 rap hit featured a sample of "Under Pressure" from Queen and David Bowie?',
-  //   answers: [
-  //     "Cool As Ice",
-  //     "Ice Ice Baby",
-  //     "Fight The Power",
-  //     "Play That Funky Music"
-  //   ],
-  //   correct: 1
-  // },
-  // {
-  //   title:
-  //     'Ironic was one of five hit singles from which artist\'s "Jagged Little Pill" album?',
-  //   answers: [
-  //     "Celine Dion",
-  //     "Alanis Morissette",
-  //     "Mariah Carey",
-  //     "Martha Wash"
-  //   ],
-  //   correct: 1
-  // },
-  // {
-  //   title:
-  //     "Dr. Dre's \"Nuthin' But a 'G' Thang\" featured an appearance by what up-and-coming rapper?",
-  //   answers: ["Jay-Z", "Nas", "Snoop Doggy Dogg", "Tupac Shakur"],
-  //   correct: 2
-  // },
-  // {
-  //   title:
-  //     "The Latin duo Los Del Rio popularized what dance craze with a 1996 Spanish-language hit?",
-  //   answers: [
-  //     "The Macarena",
-  //     "The Cha-Cha Slide",
-  //     "The Chicken Dance",
-  //     "The Achy Breaky"
-  //   ],
-  //   correct: 0
-  // }
 ];
 
 $(document).ready(function() {
